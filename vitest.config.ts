07d7b491cf4dd7c487dae03c42a8595cea366f41
dmitry-salnikov <salnykov.dv@gmail.com>
2025-05-09 16:04:07 +0300

    upd vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: ['./src/test/*.test.ts'],
     environment: 'jsdom',
     setupFiles: ['src/test/setup-tests.ts'],
     coverage: {
